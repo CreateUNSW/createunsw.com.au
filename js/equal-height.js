4e2763247804fa945632270e5758d7e70357401e
Jack Robbers <jack@2pisoftware.com>
2020-02-05 14:49:20 +1100

    apply flexbox to projects and team

--- js/equal-height.js
+++ js/equal-height.js
@@ -3,34 +3,34 @@ $(document).ready(function() {
   });
 
 var matchHeight = function () {
-  
+
     function init() {
       eventListeners();
       matchHeight();
     }
-    
+
     function eventListeners(){
       $(window).on('resize', function() {
         matchHeight();
       });
     }
-    
+
     function matchHeight(){
       var groupName = $('.columns');
       var groupHeights = [];
-      
+
       groupName.css('min-height', 'auto');
-      
+
       groupName.each(function() {
         groupHeights.push($(this).outerHeight());
       });
-      
+
       var maxHeight = Math.max.apply(null, groupHeights);
       groupName.css('min-height', maxHeight);
     };
-    
+
     return {
       init: init
     };
-    
+
   } ();
