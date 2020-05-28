e9827b526809a06576293a857c6b615e3f5d4738
Jack Robbers <jack@2pisoftware.com>
2020-05-28 23:43:20 +1000

    overhaul catalogue search and filtering

--- js/catalogue.js
+++ js/catalogue.js
@@ -1,78 +1,73 @@
-// category
-function filterSelection(c) {
-    var search = document.getElementById("search");
-    var x = document.getElementsByClassName("filterDiv");
-    var regex = new RegExp(search.value, "i");
-    if (c == "all") {
-        for (var i = 0; i < x.length; i++) {
-            x[i].style.display = "block";
-        }
-    } else {
-        search.value = "";
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-            if (x[i].className.indexOf(c) > -1) { x[i].style.display = "block"; }
+var categories = [];
+var products = [];
+
+/**
+ * @returns {Array<string>} the currently selected categories
+ */
+function getSelectedCategories() {
+    return categories.filter(category => category.state == true).map(category => category.name);
+}
+
+function getMatchedProducts() {
+    return products.filter(product => product.matched == true).map(product => product.name)
+}
+
+function showProducts() {
+    selected = getSelectedCategories();
+    matched = getMatchedProducts();
+    for (product of document.getElementsByClassName('sales-product')) {
+        if (
+            (intersection(selected, product.classList).length == 0 && selected.length > 0) ||
+            (!matched.includes(product.id))
+        ) {
+            product.classList.add('filtered');
+        } else {
+            product.classList.remove('filtered');
         }
     }
 }
 
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-    }
+function toggleCategory(toggler) {
+    const index = categories.findIndex(category => category.name == toggler)
+    console.log(index)
+    categories[index]['state'] = !categories[index]['state'];
+    var toggle =
+    document.getElementById(toggler)
+    toggle.classList.toggle('btn-secondary');
+    toggle.classList.toggle('btn-primary')
+    showProducts();
 }
 
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
+function getAlphanumericChars(s) {
+    const match = s.match(/([0-9a-zA-Z])/g);
+    if (match) {
+        return match.join("");
     }
-    element.className = arr1.join(" ");
+    return "";
 }
 
-function searchFilter() {
-    var products = document.getElementsByClassName("filterDiv");
-    const search = document.getElementById("search").value;
-    var btn = document.getElementsByClassName("active");
-    var regex = new RegExp(search, "i");
-    if (search === "" && btn[0].innerText === "Show all") {
-        for (i = 0; i < products.length; i++) {
-            products[i].style.display = "block";
-        }
-    } else if (btn[0].innerText === "Show all") {
-        for (i = 0; i < products.length; i++) {
-            products[i].style.display = "none";
-            if (products[i].innerText.match(regex) !== null) {
-                products[i].style.display = "block";
-            }
-        }
-    } else {
-        var regex1 = new RegExp(btn[0].innerText, "i");
-        for (i = 0; i < products.length; i++) {
-            products[i].style.display = "none";
-            if (products[i].innerText.match(regex) !== null && products[i].className.match(regex1) !== null) {
-                products[i].style.display = "block";
-            }
+function search() {
+    const query = document.getElementById("search").value
+    for ([index, product] of products.entries()) {
+        if (getAlphanumericChars(product.name).includes(getAlphanumericChars(query))) {
+            products[index].matched = true;
+        } else {
+            products[index].matched = false;
         }
     }
+    showProducts();
 }
 
 window.onload = function () {
-    filterSelection("all")
-    var btnContainer = document.getElementById("myBtnContainer");
-    var btns = btnContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function () {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
+    for (category of document.getElementsByClassName("sales-category")) {
+        this.categories.push(
+            {name: category.id, state: false}
+        );
+    }
+
+    for (product of document.getElementsByClassName("sales-product")) {
+        this.products.push(
+            {name: product.id, matched: false}
+        );
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 window.onload = function () {
-  var acc = document.getElementsByClassName("accordion");
-  for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-      this.classList.toggle("active");
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+        });
+    }
 };
+
+
+/**
+ * Finds the intersection of 2 arrays
+ * @param {array} a
+ * @param {array|classList} b
+ * @return {array} the intersection of the 2 arrays
+ *
+ * @see https://medium.com/@alvaro.saburido/set-theory-for-arrays-in-es6-eb2f20a61848
+ */
+function intersection(a, b) {
+    if (Array.isArray(b)) {
+        return a.filter(x => b.includes(x));
+    } else {
+        return a.filter(x => b.contains(x));
+    }
+
+}
