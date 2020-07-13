00672a97cb0ed261fdfb29144a55cfe7fdab717f
Jack Robbers <jack@2pisoftware.com>
2020-07-13 17:13:45 +1000

    fix sales map sizing and add include-media sass library

--- js/store.js
+++ js/store.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var mymap = L.map('salesmap').setView([-33.917, 151.230], 17);
+    var map = L.map('salesmap').setView([-33.917, 151.230], 17);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -7,9 +7,11 @@ window.onload = function () {
         tileSize: 512,
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoiY3JlYXRldW5zdyIsImEiOiJja2M1a3BuN2MwMDEwMnJvNDVzMTFjbTNqIn0.8mHAgT3X3r2hiqywevLQeQ'
-    }).addTo(mymap);
-    var marker = L.marker([-33.918017, 151.231467]).addTo(mymap);
-    marker.bindTooltip("Globe Lawn").openTooltip();
-    var marker = L.marker([-33.916387, 151.228506]).addTo(mymap);
-    marker.bindTooltip("MCIC Makerspace").openTooltip();
+    }).addTo(map);
+    var globe = L.marker([-33.918017, 151.231467]).addTo(map);
+    globe.bindTooltip("Globe Lawn").openTooltip();
+    var mcic = L.marker([-33.916387, 151.228506]).addTo(map);
+    mcic.bindTooltip("MCIC Makerspace").openTooltip();
+    var group = new L.featureGroup([globe, mcic]);
+    map.fitBounds(group.getBounds());
 }
